e12aa5a95f30553e71fee6ac96a39f187378f1ca
Maximilian Algehed <m.algehed@gmail.com>
2018-05-09 13:25:53 +0200

    Publish

--- makePapersList.sh
+++ makePapersList.sh
@@ -1,1 +1,3 @@
-cd papers && tree -H './papers' -T 'Papers' -L 1 --noreport --charset utf-8 -o '../papers.html'
+echo '<html><body>' > papers.html
+ls papers | sed 's/^.*/<a href="papers\/&">&<\/a><br\/>/' >> papers.html
+echo '</body></html>' >> papers.html
