d139f59deba11a4217f7c44d9457c3940420e09f
Maximilian Algehed <m.algehed@gmail.com>
2018-05-09 13:15:55 +0200

    Publish

--- makePapersList.sh
+++ makePapersList.sh
@@ -1,1 +1,1 @@
-cd papers && tree -C '/../papers.css' -H './papers' -T 'Papers' -L 1 --noreport --charset utf-8 -o '../papers.html'
+cd papers && tree -H './papers' -T 'Papers' -L 1 --noreport --charset utf-8 -o '../papers.html'
