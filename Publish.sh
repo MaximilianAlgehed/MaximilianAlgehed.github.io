0eab4fd21c79906a4ffbf3cc95d5a7a4d7cddbd3
Maximilian Algehed <m.algehed@gmail.com>
2018-05-14 21:45:31 +0200

    Publish

--- Publish.sh
+++ Publish.sh
@@ -1,4 +1,8 @@
+pandox index.md > index.html
 ./makePapersList.sh
+./makeProjectsList.sh
 git add papers/*.pdf
+git add projects/*.md
+git add projectsHTML/*.html
 git commit -am "Publish"
 git push
---
+++ projects/VisPar.md
@@ -0,0 +1,3 @@
+# VisPar - Visualising the dataflow graphs of the par monad 
+
+The paper is available [papers/VisPar-fhpc2017.pdf](here).
