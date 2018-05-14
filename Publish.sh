a7b5b5d3942474147ebf5465eb793e37865a897e
Maximilian Algehed <m.algehed@gmail.com>
2018-05-14 21:59:04 +0200

    Publish

--- Publish.sh
+++ Publish.sh
@@ -1,5 +1,5 @@
-pandoc -f markdown index.md -t html > index.html
-pandoc -f markdown projects.md -t html > projects.html
+pandoc -f markdown index.md -t html #> index.html
+pandoc -f markdown projects.md -t html #> projects.html
 ./makePapersList.sh
 ./makeProjectsList.sh
 git add papers/*.pdf
